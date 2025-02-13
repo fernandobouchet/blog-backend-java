b8d92caa022a42010edd0a68907fb5ddcfb27fa4
fernandobouchet <fernandobouchet@gmail.com>
2025-02-12 22:52:23 -0300

    feat: add 'tag' and 'post' relationship

--- src/main/java/com/fernandobouchet/blog/domain/entities/Post.java
+++ src/main/java/com/fernandobouchet/blog/domain/entities/Post.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -45,6 +47,14 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
