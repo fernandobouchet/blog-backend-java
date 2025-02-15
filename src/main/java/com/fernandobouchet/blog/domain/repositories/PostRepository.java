ec2d36826370911b7177e6daadc306a173254ce0
fernandobouchet <fernandobouchet@gmail.com>
2025-02-15 18:14:34 -0300

    feat: create repository interfaces

---
+++ src/main/java/com/fernandobouchet/blog/domain/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.fernandobouchet.blog.domain.repositories;
+
+import com.fernandobouchet.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
