7d7baa55932604c0b041777d3089146a6c986bb3
fernandobouchet <fernandobouchet@gmail.com>
2025-02-21 00:21:18 -0300

    feat: add 'tags' controllers

--- src/main/java/com/fernandobouchet/blog/controllers/TagController.java
+++ src/main/java/com/fernandobouchet/blog/controllers/TagController.java
@@ -1,16 +1,17 @@
 package com.fernandobouchet.blog.controllers;
 
+import com.fernandobouchet.blog.domain.dtos.CreateTagsRequest;
 import com.fernandobouchet.blog.domain.dtos.TagResponse;
 import com.fernandobouchet.blog.domain.entities.Tag;
 import com.fernandobouchet.blog.mappers.TagMapper;
 import com.fernandobouchet.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tags")
@@ -23,5 +24,21 @@ public class TagController {
     public ResponseEntity<List<TagResponse>> getAllTags() {
         List<Tag> tags = tagService.getAllTags();
         return ResponseEntity.ok(tagMapper.toTagResponseList(tags));
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponse = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponse,
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/fernandobouchet/blog/services/TagService.java
+++ src/main/java/com/fernandobouchet/blog/services/TagService.java
@@ -3,7 +3,11 @@ package com.fernandobouchet.blog.services;
 import com.fernandobouchet.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getAllTags();
+    List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
 }
